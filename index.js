da0eee7e252fb61cba3e611a458eda1cb0a295ce
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-02-02 11:27:05 +0200

    Osa3 Tehtava 3.11

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json()) 
+app.use(express.static('build'))
 
 morgan.token('person', function(req, res){return JSON.stringify(req.body)})
 
