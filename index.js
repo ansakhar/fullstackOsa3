f89b49d4886264376ef3f01b894d92ec4a532a39
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-02-07 00:01:04 +0200

    Osa3 Tehtava 3.19 ja 3.20

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const generateId = () => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name) {
+ /* if (!body.name) {
     console.log('error name missing')
     return response.status(400).json({ 
       error: 'name missing' 
@@ -84,7 +84,7 @@ app.post('/api/persons', (request, response, next) => {
     return response.status(400).json({ 
       error: 'number missing' 
     })
-  }
+  }*/
    /*if (persons.find(person => person.name === body.name)) {
     return response.status(400).json({ 
       error: 'name must be unique' 
@@ -97,10 +97,12 @@ app.post('/api/persons', (request, response, next) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-
+  person.save()
+  .then(savedPerson => savedPerson.toJSON())
+  .then(savedAndFormattedPerson => {
+    response.json(savedAndFormattedPerson)
+  }) 
+  .catch(error => next(error))
 })
 
 /*app.delete('/api/persons/:id', (request, response) => {
@@ -144,6 +146,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
